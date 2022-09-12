1fdc35e03387ec75b31f3e7947c6e16fc6f9e714
Andrea Grillo <andreagrillo@KPDOIT27.local>
2022-09-12 12:50:31 +0200

    exec cmd

--- vars/myfunc.groovy
+++ vars/myfunc.groovy
@@ -1,5 +1,11 @@
-#!/usr/bin/env groovy
+import hudson.util.Secret
 
-def call() {
-    echo "Hello"
+def call(String token, String addr) {
+    def cmd = "curl --silent --header \"X-Vault-Token: ${token}\" -H \"X-Vault-Wrap-Ttl: 300s\" --request POST \"${addr}/v1/auth/pipeline/role/pipeline-role/secret-id\""
+    def sout = new StringBuffer(), serr = new StringBuffer()
+    def proc = cmd.execute()
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitForOrKill(1000)
+    println sout
+    def jsonSlurper = new JsonSlurper();
 }
